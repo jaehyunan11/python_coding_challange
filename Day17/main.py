9f744e3f3ac1c10e1afa1a456455f2be3025d36f
jaehyun <jaehyuna11@gmail.com>
2021-09-03 17:10:36 -0700

    Commit test from Pycharm

---
+++ Day17/main.py
@@ -0,0 +1,15 @@
+class User:
+    # Initialize or Constructor
+    def __init__(self):
+        print("New user being created...")
+
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "angela"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.name = "jack"
